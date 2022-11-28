27571b115fa2a02b243a3d7fffb8a19ba6ff05d8
hossam <elqersh.mail@gmail.com>
2022-11-28 15:50:50 +0200

    handling errors

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, logout_user, login_required, login_user
 from werkzeug.urls import url_parse
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, EditProfileForm
 from app.models import User
 from app import db
 from app.forms import RegistrationForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -47,16 +50,52 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username= form.username.data, email= form.email.data)
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash("Congratulations you are a member in our microblog !!")
         return redirect(url_for('login'))
     return render_template('register.html', form=form, title='Register')
+
+
+# Handling user after login
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', posts=posts, user=user, title='Profile')
+
+
+@app.route('/edit_profile', methods=['POST', 'GET'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Changes Saved !")
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form, title='Edit Profile')
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
