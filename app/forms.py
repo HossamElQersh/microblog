0e5b2854bdada2e5d417cd770eb6064801fcb188
hossam <elqersh.mail@gmail.com>
2022-12-01 00:38:39 +0200

    removing typos

--- app/forms.py
+++ app/forms.py
@@ -12,6 +12,15 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Sign In')
 
 
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Post')
+
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -38,7 +47,6 @@ class EditProfileForm(FlaskForm):
     def __init__(self, original_username, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
         self.original_username = original_username
-
 
     def validate_username(self, username):
         if username.data != self.original_username:
--- app/routes.py
+++ app/routes.py
@@ -4,27 +4,41 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, logout_user, login_required, login_user
 from werkzeug.urls import url_parse
 from app import app
-from app.forms import LoginForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
 from app import db
 from app.forms import RegistrationForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, user_id=current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        flash('Posted!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', form=form, title='Home', posts=posts, next_url=next_url, prev_url=prev_url)
+
+
+# Explore
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', posts=posts, title='Explore', next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -71,13 +85,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', posts=posts, user=user, title='Profile')
-
-
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=form)
 @app.route('/edit_profile', methods=['POST', 'GET'])
 @login_required
 def edit_profile():
@@ -99,3 +116,49 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+
+# Following and UnFollowing Users
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not Found!'.format(username))
+            return redirect(url_for('index'))
+
+        if user is current_user:
+            flash('You cann\'t follow yourself')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {} Now!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not Found!'.format(username))
+            return redirect(url_for('index'))
+        if user is current_user:
+            flash('You cann\'t unfollow yourself')
+            return redirect(url_for('user', username))
+        # if not current_user.isFollowing(user):
+        #     flash('You are not followin {}'.format(username))
+        #     return redirect(url_for('user',username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('Not following {}'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
