6ee969b21e63298217a17f9d3635d6d637618eb3
hossam <elqersh.mail@gmail.com>
2022-12-19 12:45:01 +0200

    creating dynamic notification bar

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime, time
 from hashlib import md5
 
@@ -92,6 +93,7 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -112,3 +114,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
